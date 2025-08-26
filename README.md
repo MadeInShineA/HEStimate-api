aeb80aa6c8b5100c12d247c7a8ddc666e8a60cd1
GDbateaux <111980428+GDbateaux@users.noreply.github.com>
2025-08-26 15:31:00 +0200

    Face recognition (#3)
    * face_recognition API
    
    * Add Readme
    
    * Added test case
    
    * Add tests to Readme
    
    * Add error messages
    
    ---------
    
    Co-authored-by: MadeInShineA <olivier.amacker@netplus.ch>

---
+++ README.md
@@ -0,0 +1,42 @@
+# HEStimate API
+
+FastAPI API for **face verification and comparison** using DeepFace.
+
+## Prerequisites
+
+- [Python 3.11+](https://www.python.org/)
+---
+
+## Installation
+
+- Create a venv and activate it following the [python doc](https://docs.python.org/3/library/venv.html)
+- Install the requirements
+
+```
+pip install -r requirements.txt
+```
+
+## Run the API locally
+```
+fastapi dev main.py 
+```
+
+The API will be available at:
+
+```
+http://127.0.0.1:8000/
+```
+
+## Documentation
+FastAPI automatically generates an interactive documentation at:
+```
+http://127.0.0.1:8000/docs
+```
+
+## Unit Tests
+
+Run the tests:
+
+```
+pytest -q
+```
--- main.py
+++ main.py
@@ -1,8 +1,59 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from deepface import DeepFace
 
 app = FastAPI()
 
 
+# ---------- Models
+class VerifyRequest(BaseModel):
+    image: str
+
+
+class CompareRequest(BaseModel):
+    image1: str
+    image2: str
+
+
+class VerifyResponse(BaseModel):
+    success: bool
+    message: str
+
+
+# ---------- Routes
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Welcome to the HEStimate API, please check /docs for the doc"}
+
+
+@app.post("/verify", status_code=200, response_model=VerifyResponse)
+async def verify(payload: VerifyRequest):
+    faces = DeepFace.extract_faces(img_path=payload.image, anti_spoofing=True)
+    if len(faces) != 1:
+        raise HTTPException(
+            status_code=400, detail="Please take a picture with exactly 1 person"
+        )
+    elif faces[0]["is_real"] is False:
+        raise HTTPException(
+            status_code=400,
+            detail="Spoofing detected. Please provide a real face image.",
+        )
+    elif faces[0]["is_real"] is None:
+        raise HTTPException(
+            status_code=400, detail="Anti-spoofing check unavailable for this image."
+        )
+    else:
+        return {"success": True, "message": "Face verified and appears real"}
+
+
+@app.post("/compare", status_code=200)
+async def compare(payload: CompareRequest) -> bool:
+    try:
+        result = DeepFace.verify(img1_path=payload.image1, image2=payload.image2)
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=f"Comparison failed: {str(e)}")
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=f"Comparison failed: {str(e)}")
+    except FileNotFoundError as e:
+        raise HTTPException(status_code=400, detail=f"File not found: {str(e)}")
+    return result["verified"]
