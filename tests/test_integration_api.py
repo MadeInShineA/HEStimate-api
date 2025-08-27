9805d98c74a40503cf1a88e4065b363bd94056d9
GDbateaux <yolan.savioz@hes-so.ch>
2025-08-27 10:31:29 +0200

    remove print

--- tests/test_integration_api.py
+++ tests/test_integration_api.py
@@ -119,7 +119,6 @@ def test_compare_different_persons():
     img1_b64 = load_image_as_base64("tests/images/simon.jpg")
     img2_b64 = load_image_as_base64("tests/images/olivier.jpg")
     resp = client.post("/compare", json={"image1": img1_b64, "image2": img2_b64})
-    print(resp)
     assert resp.status_code == 400
     data = resp.json()
     assert "Faces do not match" in data["detail"]
